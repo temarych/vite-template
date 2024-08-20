1a3c00d4e38d5a068e5e9d419f5f853113dd529e
temarych <teemest06@icloud.com>
2024-08-20 12:37:57 +0300

    feat(setup): setup project

---
+++ src/App.tsx
@@ -0,0 +1,1 @@
+export const App = () => <h1>Home</h1>;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@modules': '/src/modules',
+      '@components': '/src/components',
+      '@store': '/src/store',
+      '@typings': '/src/typings',
+      '@utils': '/src/utils',
+      '@': '/src',
+    },
+  },
+});
