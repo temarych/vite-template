1d6cea2fab7af0b7a446e88fdc2ae8caf7d22b89
temarych <teemest06@icloud.com>
2024-08-20 16:11:38 +0300

    feat(theme): update palette

--- src/theme/palettes/darkPalette.ts
+++ src/theme/palettes/darkPalette.ts
@@ -2,9 +2,9 @@ import { PaletteOptions } from '@mui/material';
 
 export const darkPalette: PaletteOptions = {
   primary: {
-    main: '#00b4d8',
-    dark: '#0096c7',
-    light: '#48cae4',
+    main: '#9d4edd',
+    dark: '#7b2cbf',
+    light: '#c77dff',
     contrastText: '#ffffff',
   },
 };
