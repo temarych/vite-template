808299fc1fc46a7a67b9782ba91f58a6c031f226
temarych <teemest06@icloud.com>
2024-08-22 11:49:59 +0300

    refactor(app): minor improvements

--- src/components/AuthGuard/index.tsx
+++ src/components/AuthGuard/index.tsx
@@ -1,10 +1,10 @@
-import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 import { routes } from '@config/routes';
 import { selectIsAuthenticated } from '@store/features/auth';
+import { useAppSelector } from '@store/index';
 
 export const AuthGuard = () => {
-  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
   if (!isAuthenticated) return <Navigate to={routes.login()} />;
   return <Outlet />;
 };
